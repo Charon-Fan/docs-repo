bdea7473c9cabfef71bfaabf39814955589e0dde
Charon-Fan <Charon.Dian@gmail.com>
2023-06-19 17:01:42 +0800

    feat: update content path

--- tina/config.ts
+++ tina/config.ts
@@ -21,9 +21,29 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        name: "Tutorial",
+        label: "Tutorial",
+        path: "docs",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "Blog",
+        label: "Blog",
+        path: "blog",
         fields: [
           {
             type: "string",
